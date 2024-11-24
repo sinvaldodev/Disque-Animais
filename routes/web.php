92717d12de5bdc1bc0b4a977a773d32c5f6c56da
sinvaldodev <sinvaldopissinatti2@gmail.com>
2024-11-24 16:48:29 -0400

    Instalado as dependencias e começando a criar a autenticação

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\LostAnimalsController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/users', [ProfileController::class, 'index'])->name('users.index');
+Route::get('/animais-perdidos', [LostAnimalsController::class, 'index'])->name('lost-animals.index');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
